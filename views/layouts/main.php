982743db9dd90b2d0076e8db8ab056b33d473c71
Gustavo G. Andrade <gugoan@gmail.com>
2015-02-16 16:57:05 -0200

    Fix ident

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -56,17 +56,18 @@ SCRIPT;
                     ['label' => '<i class="fa fa-usd"></i> '.Yii::t('app', 'Cashbook').'', 'url' => ['/cashbook/index'], 'visible' => !Yii::$app->user->isGuest,],
                     ['label' => '<i class="fa fa-bullseye"></i> '.Yii::t('app', 'Targets').'', 'url' => ['/cashbook/target'], 'visible' => !Yii::$app->user->isGuest,],
                     ['label' => '<i class="fa fa-briefcase"></i> '.Yii::t('app', 'Options').'', 'visible' => !Yii::$app->user->isGuest,
-                    'items' => [
-                         ['label' => '<i class="fa fa-briefcase"></i> '.Yii::t('app', 'Category').'', 'url' => ['category/index']],
-                         ['label' => '<i class="fa fa-briefcase"></i> '.Yii::t('app', 'Type').'', 'url' => ['/type/index']],
+                    'items' => 
+                        [
+                            ['label' => '<i class="fa fa-briefcase"></i> '.Yii::t('app', 'Category').'', 'url' => ['category/index']],
+                            ['label' => '<i class="fa fa-briefcase"></i> '.Yii::t('app', 'Type').'', 'url' => ['/type/index']],
                         ],
                     ],
                     Yii::$app->user->isGuest ?
-                        //['label' => '<i class="fa fa-lock"></i> Entrar', 'url' => ['/user/login']] :
-                        ['label' => '<span class="glyphicon glyphicon-user"></span> '.Yii::t('app', 'Create an account').'', 'url' => ['/user/register']] :
-                        ['label' => '<i class="fa fa-unlock"></i> '.Yii::t('app', 'Sign Out').' (' . Yii::$app->user->displayName . ')',
-                            'url' => ['/user/logout'],
-                            'linkOptions' => ['data-method' => 'post']],
+                    //['label' => '<i class="fa fa-lock"></i> Entrar', 'url' => ['/user/login']] :
+                    ['label' => '<span class="glyphicon glyphicon-user"></span> '.Yii::t('app', 'Create an account').'', 'url' => ['/user/register']] :
+                    ['label' => '<i class="fa fa-unlock"></i> '.Yii::t('app', 'Sign Out').' (' . Yii::$app->user->displayName . ')',
+                        'url' => ['/user/logout'],
+                        'linkOptions' => ['data-method' => 'post']],
                 ],
             ]);
             NavBar::end();
