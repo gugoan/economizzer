b6cd19ccc8901d65eb399adcd624f7db405b8b9f
Gustavo G. Andrade <gugoan@hotmail.com>
2016-11-20 23:18:31 -0200

    test internationalization

--- components/LanguageSelector.php
+++ components/LanguageSelector.php
@@ -18,16 +18,22 @@ class LanguageSelector implements BootstrapInterface
 
             Yii::$app->language = Yii::$app->user->identity->profile->language;
             Yii::$app->defaultRoute = Yii::$app->user->identity->profile->startpage;
+            Yii::$app->formatter->currencyCode = Yii::$app->user->identity->profile->currencycode;
+            Yii::$app->formatter->decimalSeparator = Yii::$app->user->identity->profile->decimalseparator;
+
             //Yii::$app->defaultRoute = 'cashbook/overview';
             //Yii::$app->user->getIdentity()->language = Yii::$app->language;
-            // Yii::$app->formatter->defaultTimeZone = 'Europe/Malta';
-            // Yii::$app->formatter->timeZone = 'Europe/Malta';
-            // Yii::$app->formatter->dateFormat = 'php:d/m/Y';
-            // Yii::$app->formatter->datetimeFormat = 'php:d/m/Y H:i:s';
-            // Yii::$app->formatter->currencyCode = 'EUR';
-            // Yii::$app->formatter->decimalSeparator = ',';
+            //Yii::$app->formatter->defaultTimeZone = 'Europe/Malta';
+            //Yii::$app->formatter->timeZone = 'Europe/Malta';
+            //Yii::$app->formatter->dateFormat = 'php:d/m/Y';
+            //Yii::$app->formatter->datetimeFormat = 'php:d/m/Y H:i:s';
+            //Yii::$app->formatter->currencyCode = 'EUR';
+            //Yii::$app->formatter->decimalSeparator = ',';
         }else{
             Yii::$app->language = Yii::$app->request->getPreferredLanguage($this->supportedLanguages);
+            //Yii::$app->defaultRoute = Yii::$app->user->identity->profile->startpage;
+            //Yii::$app->formatter->currencyCode = 'en_US';
+            //Yii::$app->formatter->decimalSeparator = '.';
         }
     }
 }
