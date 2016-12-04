a52a710d59f9d04e273f970cf2b8e53c8f028af2
Gustavo G. Andrade <gugoan@hotmail.com>
2016-12-04 17:08:30 -0200

    update migrations

---
+++ migrations/m161204_190637_initial.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+class m161204_190637_initial extends Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m161204_190637_initial cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
