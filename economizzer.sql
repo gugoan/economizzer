49653d18ddacae3239626caa28e200757945be07
Gustavo G. Andrade <gugoan@users.noreply.github.com>
2018-07-22 10:02:40 -0300

    Update economizzer.sql

--- economizzer.sql
+++ economizzer.sql
@@ -1,11 +1,27 @@
+-- MySQL dump 10.13  Distrib 5.7.22, for Linux (x86_64)
+--
+-- Host: localhost    Database: economizzer
+-- ------------------------------------------------------
+-- Server version	5.7.22-0ubuntu18.04.1
 
-SET FOREIGN_KEY_CHECKS=0;
-SET GLOBAL sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- ----------------------------
--- Table structure for `cashbook`
--- ----------------------------
+--
+-- Table structure for table `cashbook`
+--
+
 DROP TABLE IF EXISTS `cashbook`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `cashbook` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `value` float NOT NULL,
@@ -19,19 +35,27 @@ CREATE TABLE `cashbook` (
   `category_id` int(11) NOT NULL,
   `type_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `fk_cashbook_user_id` (`user_id`),
   KEY `fk_cashbook_category_id` (`category_id`),
-  KEY `fk_cashbook_type_id` (`type_id`),
-  CONSTRAINT `fk_cashbook_tb_category` FOREIGN KEY (`category_id`) REFERENCES `category` (`id_category`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_cashbook_tb_type` FOREIGN KEY (`type_id`) REFERENCES `type` (`id_type`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_cashbook_tb_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `fk_cashbook_tb_category` FOREIGN KEY (`category_id`) REFERENCES `category` (`id_category`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB AUTO_INCREMENT=611 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='Control financial movement';
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `cashbook`
+--
 
--- ----------------------------
--- Table structure for `category`
--- ----------------------------
+LOCK TABLES `cashbook` WRITE;
+/*!40000 ALTER TABLE `cashbook` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cashbook` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `category`
+--
+
 DROP TABLE IF EXISTS `category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `category` (
   `id_category` int(11) NOT NULL AUTO_INCREMENT,
   `parent_id` int(11) DEFAULT NULL,
@@ -43,12 +67,24 @@ CREATE TABLE `category` (
   KEY `fk_tb_category_tb_user1_idx` (`user_id`),
   CONSTRAINT `fk_tb_category_tb_user1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB AUTO_INCREMENT=1010 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='Categories of entries: Water, light, card, etc.';
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `category`
+--
 
--- ----------------------------
--- Table structure for `profile`
--- ----------------------------
+LOCK TABLES `category` WRITE;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `profile`
+--
+
 DROP TABLE IF EXISTS `profile`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `profile` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
@@ -63,17 +99,25 @@ CREATE TABLE `profile` (
   KEY `tb_profile_user_id` (`user_id`) USING BTREE,
   CONSTRAINT `profile_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=158 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Records of profile
--- ----------------------------
-INSERT INTO `profile` VALUES ('1', '1', '2015-01-25 02:53:12', null, 'the one', 'en', 'cashbook/index','USD','.');
-INSERT INTO `profile` VALUES ('3', '3', '2015-02-14 03:03:28', '2015-12-24 19:55:40', 'Joe Mac', 'pt', 'cashbook/index','USD','.');
+--
+-- Dumping data for table `profile`
+--
 
--- ----------------------------
--- Table structure for `role`
--- ----------------------------
+LOCK TABLES `profile` WRITE;
+/*!40000 ALTER TABLE `profile` DISABLE KEYS */;
+INSERT INTO `profile` VALUES (1,1,'2015-01-25 04:53:12',NULL,'the one','en','cashbook/index','USD','.'),(3,3,'2015-02-14 05:03:28','2015-12-24 21:55:40','Joe Mac','pt','cashbook/index','USD','.');
+/*!40000 ALTER TABLE `profile` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `role`
+--
+
 DROP TABLE IF EXISTS `role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `role` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
@@ -82,17 +126,25 @@ CREATE TABLE `role` (
   `can_admin` smallint(6) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Records of role
--- ----------------------------
-INSERT INTO `role` VALUES ('1', 'Admin', '2015-01-25 02:53:11', null, '1');
-INSERT INTO `role` VALUES ('2', 'User', '2015-01-25 02:53:11', null, '0');
+--
+-- Dumping data for table `role`
+--
 
--- ----------------------------
--- Table structure for `type`
--- ----------------------------
+LOCK TABLES `role` WRITE;
+/*!40000 ALTER TABLE `role` DISABLE KEYS */;
+INSERT INTO `role` VALUES (1,'Admin','2015-01-25 04:53:11',NULL,1),(2,'User','2015-01-25 04:53:11',NULL,0);
+/*!40000 ALTER TABLE `role` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `type`
+--
+
 DROP TABLE IF EXISTS `type`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `type` (
   `id_type` int(11) NOT NULL AUTO_INCREMENT,
   `desc_type` varchar(45) CHARACTER SET latin1 NOT NULL,
@@ -100,17 +152,25 @@ CREATE TABLE `type` (
   `icon_type` varchar(45) CHARACTER SET latin1 DEFAULT NULL,
   PRIMARY KEY (`id_type`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='Movement Type: Debit, Credit';
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Records of type
--- ----------------------------
-INSERT INTO `type` VALUES ('1', 'Revenue', '#18bc9c', '');
-INSERT INTO `type` VALUES ('2', 'Expense', '#e74c3c', '');
+--
+-- Dumping data for table `type`
+--
 
--- ----------------------------
--- Table structure for `user`
--- ----------------------------
+LOCK TABLES `type` WRITE;
+/*!40000 ALTER TABLE `type` DISABLE KEYS */;
+INSERT INTO `type` VALUES (1,'Revenue','#18bc9c',''),(2,'Expense','#e74c3c','');
+/*!40000 ALTER TABLE `type` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
 DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `role_id` int(11) NOT NULL,
@@ -134,17 +194,25 @@ CREATE TABLE `user` (
   KEY `tb_user_role_id` (`role_id`) USING BTREE,
   CONSTRAINT `user_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=158 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Records of user
--- ----------------------------
-INSERT INTO `user` VALUES ('1', '1', '1', 'neo@neo.com', null, 'administrador', '$2y$13$cRwqEDU2Elh6KLYfPzLVkuoGwNlUvi8JEUSHXc.0gMQ1fd4IqQSFm', 'ub1TTuSVSATn3NXbuVh4bhR-m2EXgVT0', 'Ahc7a0TXH6Gqe_8GTi1UlZEWVxHsOLcv', '189.3.54.36', '2015-10-23 01:13:36', null, '2015-01-25 02:53:12', '2015-10-06 06:24:53', null, null);
-INSERT INTO `user` VALUES ('3', '2', '1', 'joe@mymail.com', null, 'joe', '$2y$13$35B0rxLTERV8nMkmL613oeCHbOs7/2dAhCd3UhIrw44tWmcQKOE9C', 'qA_US4c8xUCucUDWXyYC-SA1qcxCiGTV', 'GWQuKuPFhAJTjE8GL5w2tvaveKs1Bvno', '127.0.0.1', '2015-12-26 20:37:20', '127.0.0.1', '2015-02-14 03:03:28', '2015-10-09 17:05:45', null, null);
+--
+-- Dumping data for table `user`
+--
 
--- ----------------------------
--- Table structure for `user_auth`
--- ----------------------------
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,1,1,'neo@neo.com',NULL,'administrador','$2y$13$cRwqEDU2Elh6KLYfPzLVkuoGwNlUvi8JEUSHXc.0gMQ1fd4IqQSFm','ub1TTuSVSATn3NXbuVh4bhR-m2EXgVT0','Ahc7a0TXH6Gqe_8GTi1UlZEWVxHsOLcv','189.3.54.36','2015-10-23 03:13:36',NULL,'2015-01-25 04:53:12','2015-10-06 09:24:53',NULL,NULL),(3,2,1,'joe@mymail.com',NULL,'joe','$2y$13$35B0rxLTERV8nMkmL613oeCHbOs7/2dAhCd3UhIrw44tWmcQKOE9C','qA_US4c8xUCucUDWXyYC-SA1qcxCiGTV','GWQuKuPFhAJTjE8GL5w2tvaveKs1Bvno','127.0.0.1','2018-07-22 15:54:07','127.0.0.1','2015-02-14 05:03:28','2015-10-09 20:05:45',NULL,NULL);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_auth`
+--
+
 DROP TABLE IF EXISTS `user_auth`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user_auth` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
@@ -158,11 +226,24 @@ CREATE TABLE `user_auth` (
   KEY `tb_user_auth_user_id` (`user_id`) USING BTREE,
   CONSTRAINT `user_auth_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Table structure for `user_key`
--- ----------------------------
+--
+-- Dumping data for table `user_auth`
+--
+
+LOCK TABLES `user_auth` WRITE;
+/*!40000 ALTER TABLE `user_auth` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_auth` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_key`
+--
+
 DROP TABLE IF EXISTS `user_key`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user_key` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
@@ -176,9 +257,25 @@ CREATE TABLE `user_key` (
   KEY `tb_user_key_user_id` (`user_id`) USING BTREE,
   CONSTRAINT `user_key_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=135 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Records of user_key
--- ----------------------------
-INSERT INTO `user_key` VALUES ('1', '3', '1', 'fgX4vB8x1UW8uGdCaKQTj9kKo7Kfbltp', '2015-02-14 03:03:28', null, null);
-INSERT INTO `user_key` VALUES ('3', '3', '2', 'jkApCoj4zQ1osBGZ6gEwIJ9zgLeiOFxb', '2015-04-21 19:15:00', null, '2015-10-09 17:05:45');
+--
+-- Dumping data for table `user_key`
+--
+
+LOCK TABLES `user_key` WRITE;
+/*!40000 ALTER TABLE `user_key` DISABLE KEYS */;
+INSERT INTO `user_key` VALUES (1,3,1,'fgX4vB8x1UW8uGdCaKQTj9kKo7Kfbltp','2015-02-14 05:03:28',NULL,NULL),(3,3,2,'jkApCoj4zQ1osBGZ6gEwIJ9zgLeiOFxb','2015-04-21 22:15:00',NULL,'2015-10-09 20:05:45');
+/*!40000 ALTER TABLE `user_key` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-07-22  9:59:43
